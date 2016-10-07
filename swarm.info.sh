85175a872e5c26e57e1f340c2a19aea5f3598773
scizeron <stephane.cizeron@gmail.com>
2016-10-07 14:11:55 +0000

    updates

--- swarm.info.sh
+++ swarm.info.sh
@@ -1,99 +1,125 @@
-#!/bin/sh
-PROG=`basename "$0"`
+#!/bin/bash
+
+declare -r prog=`basename "$0"`
+declare -r dockerRemoteApiPort=2375
+declare -A hostsMap
 
 if [ $# -eq 0 ]
 then
- echo "The resource group is mandatory :  $PROG <resource_group_name>"
+ echo "The resource group is mandatory :  $prog <resource_group_name>"
  exit 1
-else 
- RESOURCE_GRP_NAME=$1
+else
+ resourceGroupName=$1
 fi
 
-HOSTS=$(azure vm list --json -g $RESOURCE_GRP_NAME | jq .[].name | sed ':a;N;$!ba;s/\n/ /g' | sed 's/"//g')
+hosts=$(azure vm list --json -g $resourceGroupName | jq .[].name | sed ':a;N;$!ba;s/\n/ /g' | sed 's/"//g')
 
 echo ""
 
-for HOST in $HOSTS
+for host in $hosts
 do
- HOST_INFO=$(curl -s $HOST:2375/info)
- NODE_ID=$(echo $HOST_INFO | jq .Swarm.NodeID | sed 's/"//g')
+ hostInfo=$(curl -s $host:$dockerRemoteApiPort/info)
+ nodeId=$(echo $hostInfo | jq .Swarm.NodeID | sed 's/"//g')
 
- if [ -z $NODE_ID ]
+ if [ -z $nodeId ]
  then
   continue
  fi
 
- NODE_ADDR=$(echo $HOST_INFO | jq .Swarm.NodeAddr | sed 's/"//g')
- CLUSTER_ID=$(echo $HOST_INFO | jq .Swarm.Cluster.ID | sed 's/"//g')
+ nodeAddr=$(echo $hostInfo | jq .Swarm.NodeAddr | sed 's/"//g')
+ clusterId=$(echo $hostInfo | jq .Swarm.Cluster.ID | sed 's/"//g')
 
  echo "========================================================================"
- echo " - HOST       : $HOST"
- echo " - NODE_ID    : $NODE_ID"
- echo " - NODE_ADDR  : $NODE_ADDR"
+ echo " - host       : $host"
+ echo " - nodeId     : $nodeId"
+ echo " - nodeAddr   : $nodeAddr"
 
- if [ "$CLUSTER_ID" != "" ]
+ if [ "$clusterId" != "" ]
  then
-   echo " - CLUSTER_ID : $CLUSTER_ID"
-   SERVICES=$(curl -s $HOST:2375/services)
-   SERVICES_LEN=$(echo $SERVICES | jq length)
-   SERVICE_IDS=$(echo $SERVICES | jq .[].ID | sed ':a;N;$!ba;s/\n/ /g' | sed 's/"//g')
-   echo " - SERVICES   : $SERVICES_LEN"
+   echo " - clusterId  : $clusterId"
 
-   for SERVICE_ID in $SERVICE_IDS
+   ##########################################################################
+   # NODES
+   ##########################################################################
+   nodes=$(curl -s $host:$dockerRemoteApiPort/nodes)
+   nodesLen=$(echo $nodes | jq length)
+   echo " - nodes      : $nodesLen"
+   nodeIdx=0
+   while [ $nodeIdx -lt $nodesLen ]
    do
-     SERVICE=$(curl -s $HOST:2375/services/$SERVICE_ID)
-     SERVICE_NAME=$(echo $SERVICE | jq .Spec.Name | sed 's/"//g')
-     IMAGE=$(echo $SERVICE | jq .Spec.TaskTemplate.ContainerSpec.Image | sed 's/"//g')
-     REPLICAS=$(echo $SERVICE | jq .Spec.Mode.Replicated.Replicas | sed 's/"//g')
-     MODE=$(echo $SERVICE | jq .Endpoint.Spec.Mode | sed 's/"//g')
-     
-     echo "  - SERVICE   : '$SERVICE_NAME' [$IMAGE] - replicas: $REPLICAS, mode:  $MODE}"
-    
-     if [ "vip" = $MODE ]
+    nodeName=$(echo $nodes | jq .[$nodeIdx].Description.Hostname | sed 's/"//g')
+    nodeId=$(echo $nodes | jq .[$nodeIdx].ID | sed 's/"//g')
+    declare "node$nodeId"=$nodeName    
+    echo "  - node      : $nodeName  - id: $nodeId"
+    nodeIdx=$(expr $nodeIdx + 1)
+   done
+
+   ##########################################################################
+   # SERVICES
+   ##########################################################################
+   services=$(curl -s $host:$dockerRemoteApiPort/services)
+   servicesLen=$(echo $services | jq length)
+   serviceIds=$(echo $services | jq .[].ID | sed ':a;N;$!ba;s/\n/ /g' | sed 's/"//g')
+   echo " - services   : $servicesLen"
+
+   for serviceId in $serviceIds
+   do
+     service=$(curl -s $host:$dockerRemoteApiPort/services/$serviceId)
+     serviceName=$(echo $service | jq .Spec.Name | sed 's/"//g')
+     image=$(echo $service | jq .Spec.TaskTemplate.ContainerSpec.Image | sed 's/"//g')
+     replicas=$(echo $service | jq .Spec.Mode.Replicated.Replicas | sed 's/"//g')
+     mode=$(echo $service | jq .Endpoint.Spec.Mode | sed 's/"//g')
+
+     echo "  - service   : '$serviceName' [$image] - replicas: $replicas, mode:  $mode}"
+
+     if [ "vip" = $mode ]
      then
-      PORT_LEN=$(echo $SERVICE | jq '.Endpoint.Ports | length')
-      PORT_IDX=0
-      while [ $PORT_IDX -lt $PORT_LEN ]
+      portsLen=$(echo $service | jq '.Endpoint.Ports | length')
+      portIdx=0
+      while [ $portIdx -lt $portsLen ]
       do
-       PORT=$(echo $SERVICE | jq .Endpoint.Ports[$PORT_IDX])
-       PUB_PORT=$(echo $PORT | jq .PublishedPort)
-       TARGET_PORT=$(echo $PORT | jq .TargetPort)
-       echo "    - PORT    : $PUB_PORT -> $TARGET_PORT"
-       PORT_IDX=$(expr $PORT_IDX + 1)
-      done    
-      VIP_LEN=$(echo $SERVICE | jq '.Endpoint.VirtualIPs | length')
-      VIP_IDX=0
-      while [ $VIP_IDX -lt $VIP_LEN ]
+       port=$(echo $service | jq .Endpoint.Ports[$portIdx])
+       pubPort=$(echo $port | jq .PublishedPort)
+       targetPort=$(echo $port | jq .TargetPort)
+       echo "   - port     : $pubPort -> $targetPort"
+       portIdx=$(expr $portIdx + 1)
+      done
+      vipsLen=$(echo $service | jq '.Endpoint.VirtualIPs | length')
+      vipIdx=0
+      while [ $vipIdx -lt $vipsLen ]
       do
-       VIRTUAL_IP=$(echo $SERVICE | jq .Endpoint.VirtualIPs[$VIP_IDX])
-       ADDR=$(echo $VIRTUAL_IP | jq .Addr | sed 's/"//g')
-       echo "    - VIP     : $ADDR"
-       VIP_IDX=$(expr $VIP_IDX + 1)
+       virtualIp=$(echo $service | jq .Endpoint.VirtualIPs[$vipIdx])
+       addr=$(echo $virtualIp | jq .Addr | sed 's/"//g')
+       echo "   - vip      : $addr"
+       vipIdx=$(expr $vipIdx + 1)
       done
      fi
 
      ##########################################################################
      # TASKS
      #########################################################################
-     TASKS=$(curl -s $HOST:2375/tasks?service=$SERVICE_NAME)
-     TASKS_LEN=$(echo $TASKS | jq '. | length')
-     TASK_IDX=0
-     while [ $TASK_IDX -lt $TASKS_LEN ]
+     tasks=$(curl -s $host:$dockerRemoteApiPort/tasks?service=$serviceName)
+     tasksLen=$(echo $tasks | jq '. | length')
+     taskIdx=0
+     while [ $taskIdx -lt $tasksLen ]
      do
-      TASK=$(echo $TASKS | jq .[$TASK_IDX])
-      NODE_ID=$(echo $TASK | jq .NodeID | sed 's/"//g')
-      STATUS=$(echo $TASK | jq .Status.State | sed 's/"//g')
-      ADDRESSES=$(echo $TASK | jq '.NetworksAttachments[].Addresses' | sed ':a;N;$!ba;s/\n/ /g' | sed 's/ //g' | sed 's/"//g')
-       echo "    - TASK     : '$STATUS' on '$NODE_ID' - IPs: $ADDRESSES"
-      TASK_IDX=$(expr $TASK_IDX + 1)
-     done 
- 
+      task=$(echo $tasks | jq .[$taskIdx])
+      nodeId=$(echo $task | jq .NodeID | sed 's/"//g')
+      nodeNameVar="node$nodeId"
+      nodeName=${!nodeNameVar}
+      status=$(echo $task | jq .Status.State | sed 's/"//g')
+      addresses=$(echo $task | jq '.NetworksAttachments[].Addresses' | sed ':a;N;$!ba;s/\n/ /g' | sed 's/ //g' | sed 's/"//g')
+      echo "   - task     : '$status' - node: $nodeName - ip(s): $addresses"
+      taskIdx=$(expr $taskIdx + 1)
+     done
+
    done
  fi
-  
+
 done
 
 echo "========================================================================"
 echo ""
 
 exit 0
+
